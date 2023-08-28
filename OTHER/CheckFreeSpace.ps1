99b131c3bfe5c20d5439ec0d5b80b30134e8a04f
Solbreen <rundinosawr@gmail.com>
2023-08-28 15:58:28 +0300

    Прошлый коммит не смотреть. Я ничего не понял. Этот коммит с нормальным описанием CheckFreeSpace

--- OTHER/CheckFreeSpace.ps1
+++ OTHER/CheckFreeSpace.ps1
@@ -1,28 +1,8 @@
 ﻿<#
 .SYNOPSIS
-<<<<<<< HEAD
     Проверка свободного места на диске С.
 .DESCRIPTION
     Проверяет место на диске С. Если свободного места менее 25%, то появится окошко, предлагающее нажать "ок" или 
-=======
-    A short one-line action-based description, e.g. 'Tests if a function is valid'
-.DESCRIPTION
-    A longer description of the function, its purpose, common use cases, etc.
-.NOTES
-    Information or caveats about the function e.g. 'This function is not supported in Linux'
-.LINK
-    Specify a URI to a help page, this will show when Get-Help -Online is used.
-.EXAMPLE
-    Test-MyTestFunction -Verbose
-    Explanation of the function or its result. You can include multiple examples with additional .EXAMPLE lines
-#>
-
-
-
-
-
-# Скрипт для проверки места на компе
->>>>>>> 4962988178582f83d723f6bb1c5958db89bbfa48
 
     нажать кнопку, которая открывает cleanmgr.exe.
 .LINK
