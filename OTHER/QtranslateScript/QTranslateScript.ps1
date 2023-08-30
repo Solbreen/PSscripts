e769a2930e9657a07b12f4862b1f9f2d18b77e28
Solbreen <rundinosawr@gmail.com>
2023-08-30 15:43:13 +0300

    Добавил настройку Qтранслейт

--- OTHER/QtranslateScript/QTranslateScript.ps1
+++ OTHER/QtranslateScript/QTranslateScript.ps1
@@ -29,15 +29,17 @@ catch {
     throw "$($_.Exception.Message)"
 }
 Expand-Archive -path $qfile  -DestinationPath $QLocation
-New-Item -Path -Name -
+New-Item -Path "$QLocation\QTRANS*" -Name Data -ItemType Directory
+$a = Invoke-WebRequest -uri 'https://raw.githubusercontent.com/Solbreen/PSscripts/main/OTHER/QtranslateScript/Options.json'
+New-Item -Path "$QLocation\QTRANS*\Data" -Name 'Options.json' -ItemType File -Value $a.content | Out-Null
 
 
-<#$Qexe = Get-Item "$env:LOCALAPPDATA\QTRANS*\*.exe"
+$Qexe = Get-Item "$env:LOCALAPPDATA\QTRANS*\*.exe"
 
 $shell = New-Object -ComObject WScript.Shell
 $lnk = $shell.CreateShortcut("$env:AppData\Microsoft\Windows\Start Menu\Programs\Startup\Q.lnk")
 $lnk.TargetPath = $Qexe.FullName
-$lnk.Save()#>
+$lnk.Save()
 
 
 
